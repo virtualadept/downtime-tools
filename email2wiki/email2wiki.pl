0fbcfbbe22ad0a9e9d89dbdad46ca4fb133e349c
corvus <corvus@b67f2acc-af4c-4995-ba3b-d0d1183edda5>
2008-01-08 06:27:06 +0000

    Eh, it sorta works

--- email2wiki/email2wiki.pl
+++ email2wiki/email2wiki.pl
@@ -1,21 +1,29 @@
 #!/usr/bin/perl
 #
 # Short program to take email threads and convert them into wiki-compatable downtimes.
-#
+# This is really really rough.
+
+use Text::Wrap;
 
 foreach $downtimeline (<STDIN>) {
 	# First, take out the annoying >'s
 	$downtimeline =~ s/>//g;
 	# Strip out leading spaces but keep wierd paragraph breaks
-	if ($downtimeline =~ /^\s+\w/) { $downtimeline =~ s/^\s+//g };
+	if ($downtimeline =~ /^\s+\S/) { $downtimeline =~ s/^\s+//g };
 	# Now take out stupid email reply headers
-	next if $downtimeline =~ /wrote\:/;
+	$downtimeline = "-----------" if $downtimeline =~ /wrote\:/;
 	next if $downtimeline =~ /-0800/;
 	next if $downtimeline =~ /<\w+\@/;
+
+	#Feed lines of text into an array until we hit a paragraph
+	push(@dtparagraph, $downtimeline);	
 	
-	if ($downtimeline !=~ /^\s+$/) {
+	# We hit a paragraph break, lets do a bunch of stuff to the array
+	if ($downtimeline =~ /^(\s|\n)+$/) {
+		chomp(@dtparagraph);
+		print wrap('','',@dtparagraph) . "\n\n";	
+		@dtparagraph = undef;
 	}
 }
 
-print @dtparagraph;
 	
